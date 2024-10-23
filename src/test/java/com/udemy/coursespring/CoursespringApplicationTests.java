83ce51f2844c071175ce35727fdf2f6e049a4981
lorenafrankiv <lorenafmenegon@gmail.com>
2024-10-22 23:30:53 -0300

    criação do projeto

---
+++ src/test/java/com/udemy/coursespring/CoursespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

429f538cbef308946d020865debf7d05a00edf9e
lorenafrankiv <lorenafmenegon@gmail.com>
2024-11-04 22:11:48 -0300

    Conexão com o Banco de Dados H2 e mapeamento da table USER

--- src/main/java/com/udemy/coursespring/CourseSpringApplication.java
+++ src/main/java/com/udemy/coursespring/CourseSpringApplication.java
@@ -1,5 +1,6 @@
 package com.udemy.coursespring;
 
+import com.udemy.coursespring.resources.UserResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
